7eec22f293ec12619a9adc4f2d6e3d99526cd79d
Camden Phalen <cphalen@hubspot.com>
2024-09-18 14:41:01 -0400

    Add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  extends: "eslint:recommended",
+  root: true,
+  env: {
+    browser: false,
+    node: true,
+    commonjs: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    "no-console": "off",
+    "no-return-await": "error",
+  },
+
+  overrides: [
+    {
+      files: ["**/__tests__/**/*.js", "**/__mocks__/**/*.js"],
+      env: {
+        jest: true,
+        node: true,
+      },
+    },
+    {
+      files: ["acceptance-tests/tests/**/*.js"],
+      env: {
+        jasmine: true,
+        node: true,
+      },
+    },
+  ],
+};
